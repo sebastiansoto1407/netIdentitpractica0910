77f70e7860be3b42d198427e92d8d6cfb71f9559
sebastiansoto1407 <sebastiansotoastete1407@gmail.com>
2025-10-14 17:30:32 -0400

    trabajorealizadoeldia0910

---
+++ netidentitycshark/netidentitycshark/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace netidentitycshark.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
